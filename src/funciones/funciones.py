7c5044cf53a570dbac0f7558a99eb08260d2992b
Galocha <Galocha@DESKTOP-NEUN9HN>
2024-10-20 20:33:35 +0200

    proceso2

--- src/funciones/funciones.py
+++ src/funciones/funciones.py
@@ -5,22 +5,49 @@ Created on 18 oct 2024
 '''
 
 from us.lsi.tools import Preconditions
+import math
+from fontTools.misc.bezierTools import epsilon
 
-
-def producto(n:int, k:int) -> int:
+def ejercicio1(n:int, k:int) -> int:
     Preconditions.check_argument(n>=k, "N tiene que ser mayor o igual que k")
     res:int = 1
     for i in range(0,k):
         res = res*(n-i+1)
     return res
 
-def producto_secuencia_geometrica(a:int, r:int, k:int) -> int:
+def ejercicio2(a:int, r:int, k:int) -> int:
     producto = 1
     for n in range(0, k):
         e = a*(r**n)
         producto = producto*e
     return producto
 
-def numero_combinatorio(n:int, k:int) -> int:
+def ejercicio3(n:int, k:int) -> int:
     Preconditions.check_argument(n>=k, "N tiene que ser mayor o igual que k")
-    
+    return math.factorial(n) // (math.factorial(k) * math.factorial(n - k))     #fórmula del número combinatorio: (n sobre k) = n!/(k!(n-k)!)
+
+def ejercicio4(n:int, k:int) -> float:
+    Preconditions.check_argument(n>=k, "N tiene que ser mayor o igual que k")
+    suma:int = 0
+    for i in range(0, k):
+        x:float = (-1)**i
+        y:float = math.factorial(k+1) // (math.factorial(i+1) * math.factorial((k+1) - (i+1)))
+        z:float = (k-i)**n
+        a:float = 1/math.factorial(k)
+        suma = suma + int(x + y + z)
+        s: float = float(a * suma)
+    return s
+
+
+def f(x:int) -> int:
+    return x**2 - 2
+
+def fp(x:int) -> int:
+    return 2*x
+
+def ejercicio5(a:float, epsilon:float , f, fp) -> float:
+    x = a
+    while abs(f(x)) > epsilon:
+        x = x - f(x) / fp(x)
+    return x
+
