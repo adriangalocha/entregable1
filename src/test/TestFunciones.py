4751ec8715244760c2464c429d43b7286bd091ec
Galocha <Galocha@DESKTOP-NEUN9HN>
2024-10-20 20:42:58 +0200

    testfunciones

--- src/test/TestFunciones.py
+++ src/test/TestFunciones.py
@@ -3,6 +3,11 @@ Created on 18 oct 2024
 
 @author: Galocha
 '''
+from funciones.funciones import ejercicio1, ejercicio2, ejercicio3, ejercicio4, ejercicio5, f, fp
 
 if __name__ == '__main__':
-    pass
+    print(ejercicio1(7, 3))
+    print(ejercicio2(3, 4, 8))
+    print(ejercicio3(7, 3))
+    print(ejercicio4(7, 3))
+    print(ejercicio5(5, 3, f, fp))
